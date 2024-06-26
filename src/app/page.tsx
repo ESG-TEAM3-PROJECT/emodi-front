06e7698fa640f7764a81f4b2f44923c0d4ee2edb
kim jihwan <kjhwan0802@naver.com>
2024-06-26 14:56:51 +0900

    add header img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import ReactCalendar from "../components/molecules/Calendar";
-import HeaderNavBar from "@/components/molecules/HeaderNavBar";
+import HeaderNavBar from "components/molecules/HeaderNavBar";
 
 export default function Home() {
   return (
--- src/components/molecules/HeaderNavBar/index.tsx
+++ src/components/molecules/HeaderNavBar/index.tsx
@@ -1,9 +1,10 @@
-import Logo from "/image/logo.svg";
-import HamburgerBtn from "/image/hamburger_btn.svg";
+import Image from "next/image";
+
 
 export default function HeaderNavBar() {
-  return(<div>
-    <Logo />
-    <HamburgerBtn />
+  return(
+  <div id="header">
+    <Image src="/image/logo.svg" width={70} height={30} alt="logo" />
+    <Image src="/image/hamburger_btn.svg" width={30} height={19} alt="hamburger_btn" />
   </div>);
 }
